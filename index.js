ef17171fd581a66b1a0fb3aa2d1ca5f5351f877b
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-03-05 13:12:40 +0100

    First blog item

---
+++ index.js
@@ -0,0 +1,69 @@
+class IndexView {
+    constructor() {
+        this.blogContainer = document.getElementById("blog");
+
+        fetch("blog.json").then(res => res.json()).then(json => {
+            json.forEach((blog) => {
+                this.blogContainer.appendChild(this.newBlog(blog));
+            });
+        });
+    }
+    
+    newBlog(blog) {
+        const div = document.createElement("div");
+        div.className = "w3-card-4 w3-margin w3-white";
+        
+        this.appendBlogTitle(div, blog);
+        this.appendBlogEntry(div, blog);
+        const hr = document.createElement("hr");
+        div.appendChild(hr);
+        
+        return div;
+    }
+    
+    appendBlogTitle(div, blog) {
+        const img = document.createElement("img");
+        img.src = blog.imageurl;
+        img.alt = blog.imagealt;
+        img.style = "width:100%";
+        
+        div.appendChild(img);
+        
+        const title = document.createElement("div");
+        
+        title.className = "w3-container";
+        const h3 = document.createElement("h3");
+        const bold = document.createElement("b");
+        bold.appendChild(document.createTextNode(blog.title));
+        h3.appendChild(bold);
+        title.appendChild(h3);
+        
+        const h5 = document.createElement("h5");
+        h5.appendChild(document.createTextNode(blog.description + ", "));
+        const span = document.createElement("span");
+        span.className = "w3-opacity";
+        span.appendChild(document.createTextNode(blog.date));
+        
+        h5.appendChild(span);
+        title.appendChild(h5);
+        
+        div.append(title);
+    }
+    appendBlogEntry(div, blog) {
+        const entry = document.createElement("div");
+        
+        entry.className = "w3-container";
+        
+        const p = document.createElement("p");
+        p.appendChild(document.createTextNode(blog.entry));
+        
+        entry.appendChild(p);
+        
+        div.append(entry);
+        
+    }
+}
+const view = new IndexView();
+
+
+
