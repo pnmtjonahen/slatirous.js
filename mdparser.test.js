709af783f3b2b78aadf010ec2bb4343e7a3d1f29
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2019-10-16 19:07:36 +0200

    updated test description

--- mdparser.test.js
+++ mdparser.test.js
@@ -12,24 +12,25 @@ describe('Markdown plus Parser', () => {
           imageurl: "",
           imagealt: ""
       };
-  it('Parse full entry', () => {
+  it('parse empty entry', () => {
       var result = mdParser.parseMd(blog, '');
       expect(result).toEqualCaseInsensitive(`<div class="blog"></div>`);
   });
-  it('code with class', () => {
+
+  it('parse code block with class', () => {
     var result = mdParser.parseMd(blog, '```code\n\n<html></html>\n\n```');
     expect(result).toEqualCaseInsensitive(`<div class="blog"><pre class="code">
 &lt;html&gt;&lt;/html&gt;
 </pre></div>`);
   });
-  it('code plain', () => {
+  it('parse code plain html', () => {
     var result = mdParser.parseMd(blog, '```\n\n<html></html>\n\n```');
     expect(result).toEqualCaseInsensitive(`<div class="blog"><pre >
 &lt;html&gt;&lt;/html&gt;
 </pre></div>`);
   });
 
-  it('headers', () => {
+  it('parse headers', () => {
     var result = mdParser.parseMd(blog,`# H1
 ## H2
 ### H3
@@ -46,17 +47,17 @@ describe('Markdown plus Parser', () => {
 #hashtag</div>`);
   });
 
-  it('headers with id', () => {
+  it('parse headers with id', () => {
     var result = mdParser.parseMd(blog,`# H1 {#myid}`);
     expect(result).toEqualCaseInsensitive(`<div class="blog"><h1 class="section">H1</h1></div>`);
   });
 
-  it('headers with nested link', () => {
+  it('parse headers with nested link', () => {
     var result = mdParser.parseMd(blog,`# H1 [blog](http://localhost) {#myid}`);
     expect(result).toEqualCaseInsensitive(`<div class="blog"><h1 class="section">H1 <a href="http://localhost" target="_blank">blog</a></h1></div>`);
   });
 
-  it('table of content', () => {
+  it('parse table of content', () => {
     var result = mdParser.parseMd(blog,`!table-of-content
 # H1 {#myid}`);
     expect(result).toEqualCaseInsensitive(`<div class="blog"><p>Table of content</p>
@@ -66,40 +67,40 @@ describe('Markdown plus Parser', () => {
 <h1 class="section back" id="id1_myid">H1<span class="back"><a class="back" onClick="document.body.scrollTop = 0; document.documentElement.scrollTop = 0; return false;">^</a></span></h1></div>`);
   });
 
-  it('img inside text', () => {
+  it('parse img inside text', () => {
     expect(mdParser.parseMd(blog,'pre text ![](img.jpg) post text'))
       .toEqualCaseInsensitive(`<div class="blog"><p>pre text <div class="blog"><img src="img.jpg" alt="" class="blog"/></div> post text</p></div>`);
   });
-  it('img with styling', () => {
+  it('parse img with styling', () => {
     expect(mdParser.parseMd(blog,'![img](img.jpg =autox100%)'))
       .toEqualCaseInsensitive(`<div class="blog"><div class="blog"><img src="img.jpg" alt="img" class="blog" style="width:auto; height:100%"/></div></div>`);
   });
-  it('img with styling fixed size', () => {
+  it('parse img with styling fixed size', () => {
     expect(mdParser.parseMd(blog,'![img](img.jpg =100x100)'))
       .toEqualCaseInsensitive(`<div class="blog"><div class="blog"><img src="img.jpg" alt="img" class="blog" style="width:100px; height:100px"/></div></div>`);
   });
-  it('img with title', () => {
+  it('parse img with title', () => {
     expect(mdParser.parseMd(blog,'![img](img.jpg "title")'))
       .toEqualCaseInsensitive(`<div class="blog"><div class="blog"><img src="img.jpg" alt="img" class="blog" title="title"/></div></div>`);
   });
-  it('link inside text', () => {
+  it('parse link inside text', () => {
     expect(mdParser.parseMd(blog,'pre text [](img.html) post text'))
       .toEqualCaseInsensitive(`<div class="blog"><p>pre text <a href="img.html" target="_blank">img.html</a> post text</p></div>`);
   });
 
-  it('link with embedded img', () => {
+  it('parse link with embedded img', () => {
     expect(mdParser.parseMd(blog,'[![img](img.jpg =autox100%)](img.html)'))
       .toEqualCaseInsensitive(`<div class="blog"><a href="img.html" target="_blank"><div class="blog"><img src="img.jpg" alt="img" class="blog" style="width:auto; height:100%"/></div></a></div>`);
   });
 
-  it('list singel', () => {
+  it('parse list, single list', () => {
     expect(mdParser.parseMd(blog,`
 - single
 `))
       .toEqualCaseInsensitive(`<div class="blog"><ul><li>single</li></ul></div>`);
   });
 
-  it('list double', () => {
+  it('parse list, double list, non list', () => {
     expect(mdParser.parseMd(blog,`
 - first
 - second
@@ -116,7 +117,7 @@ describe('Markdown plus Parser', () => {
   });
 
 
-  it('step', () => {
+  it('parse steps', () => {
     expect(mdParser.parseMd(blog,`
 -Step one
 -Step Two
@@ -129,7 +130,7 @@ describe('Markdown plus Parser', () => {
 -None</div>`);
   });
 
-  it('paragraph', () => {
+  it('parse paragraphs', () => {
     expect(mdParser.parseMd(blog,`
 Line one
 Line Two
@@ -143,14 +144,14 @@ Line Two</p>
 
   });
 
-  it('blockquotes', () => {
+  it('parse blockquotes', () => {
     expect(mdParser.parseMd(blog, `
 > block quote
 `))
     .toEqualCaseInsensitive(`<div class="blog"><blockquote><p>block quote</p></blockquote></div>`);
   });
 
- it('arcordion', () => {
+ it('parse arcordion', () => {
    expect(mdParser.parseMd(blog, `
 before
 
